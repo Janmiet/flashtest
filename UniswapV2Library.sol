f5041287c5d69c3052464a889c6c4b15bdb99edd
Janmiet <84568169+Janmiet@users.noreply.github.com>
2021-05-21 12:41:08 +0300

    Update and rename Lib.sol to UniswapV2Library.sol

--- UniswapV2Library.sol
+++ UniswapV2Library.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0;
 
 import './interfaces/IUniswapV2Pair.sol';
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import 'https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/libraries/SafeMath.sol';
 
 library UniswapV2Library {
     using SafeMath for uint;
